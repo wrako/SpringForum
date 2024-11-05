f7455910269149942391e0b664500e3b73f7b1cf
name <mysuper@gmail.com>
2024-11-05 15:41:49 +0100

    Delete Old css file

---
+++ user-service/src/main/java/com/forum/userservice/entity/VerificationToken.java
@@ -0,0 +1,23 @@
+package com.forum.userservice.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+    // Геттеры и сеттеры
+}
